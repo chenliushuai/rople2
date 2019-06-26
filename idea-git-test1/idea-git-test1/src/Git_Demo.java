92433b924b1ec0b6932d3cdf0f4734993e578bac
chenliushuai <18339838967@163.com>
2019-06-26 19:23:01 +0800

    添加本地工程

---
+++ idea-git-test1/idea-git-test1/src/Git_Demo.java
@@ -0,0 +1,9 @@
+public class Git_Demo {
+    public static void main(String[] args) {
+        System.out.println("dnjfnaikfkf");
+        System.out.println("dnjfnaikfkf");
+        System.out.println("dnjfnaikfkf");
+        System.out.println("dnjfnaikfkf");
+        System.out.println("dnjfnaikfkf");
+    }
+}
